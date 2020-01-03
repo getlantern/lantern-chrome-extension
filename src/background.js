1d03e743d4a72d2b2544ee7b92c721e30caa17aa
Myles Horton <myles@getlantern.org>
2020-01-03 09:57:01 -0800

    use baidu by default if lantern isn't running

--- src/background.js
+++ src/background.js
@@ -1,9 +1,68 @@
+var lanternRunning = false;
+
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     return {
       // If Lantern is running, we want to do something different here.
-      redirectUrl: details.url.replace("search.lantern.io", "cse.google.com")
+      redirectUrl: redirectTo(details)
     };
   },
   {urls: ["*://search.lantern.io/*"]},
   ["blocking"]);
+
+function redirectTo(details) {
+  if (lanternRunning) {
+    return details.url.replace("search.lantern.io", "cse.google.com")
+  } else {
+    // For whatever reason using a URL and searchParams doesn't work here, so parse manually.
+    const queryKey = 'gsc.q='
+    const n = details.url.search(queryKey) + queryKey.length
+    const query = details.url.substring(n)
+    return "https://www.baidu.com/s?ie=utf-8&wd="+query
+  }
+}
+
+var ws = null;
+
+function checkForMessages() {
+  if (ws == null) {
+    ws = createWebSocket();
+  }
+}
+
+function createWebSocket() {
+  const url = chrome.runtime.getURL("settings.json");
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => connect(json))
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+      lanternRunning = false;
+    });
+}
+
+function connect(settings) {
+  console.dir(settings)
+  var s = new WebSocket('ws://'+settings.uiAddr+'/'+settings.localHTTPToken+'/data');
+  s.onerror = function(event){
+    console.log("Error");
+    lanternRunning = false;
+  }
+  exampleSocket.onopen = function (event) {
+    console.log("open");
+    lanternRunning = true;
+  };
+  s.onmessage = function (event) {
+    console.log("got message from lantern")
+  };
+  s.onclose = function() {
+    // Just set the variable to null so it will be re-opened on the next pass.
+    ws = null;
+    lanternRunning = false;
+  };
+}
+
+// Disable the popup by default
+chrome.browserAction.disable();
+
+periodicCheck = setInterval(checkForMessages, 5000);
